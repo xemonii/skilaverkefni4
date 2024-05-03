8a3705f0a47474a467c2f12dccdaf0b51196e9e0
Tómas N <116449597+xemonii@users.noreply.github.com>
2024-05-03 13:05:41 +0000

    Add files via upload

---
+++ scripts/Scripts/Button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Takki : MonoBehaviour
+{
+
+    public void Byrja()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
